0b4a2e0aecf52a767a043dac5f6aba96c6130c13
Deep13 <deep.deepak30@gmail.com>
2024-05-19 00:15:49 +0530

    initial

---
+++ src/Constants.js
@@ -0,0 +1,119 @@
+export const day_title = 'Happy Govardhan Puja';
+export const banner_key1 = 'ca-app-pub-4717579333914549/7342079411';
+export const banner_key2 = 'ca-app-pub-4717579333914549/7365295999';
+export const banner_key3 = 'ca-app-pub-4717579333914549/4739132654';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/6443930717';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/5492976979';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/7173724308';
+export const language = ['English', 'Hindi'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May this special day of Govardhan Puja bring you success, wealth and love. May lord Krishna bless you and your family right from heaven above.',
+      },
+      {
+        id: '002',
+        text:
+          'I pray to God for your prosperous life. May you find all the delights of life. May your all dreams come true. My best wishes will always be with you.',
+      },
+      {
+        id: '003',
+        text:
+          'A day of festivities and bliss. Govardhan Puja is here again. May lord Krishna bring you love and luck, and destroy all the evils and pain. Happy Govardhan Puja.',
+      },
+      {
+        id: '004',
+        text:
+          'Diwali has filled this day with light. Govardhan Puja is about to start. May you be blessed all year through. That is my genuine, wish from the heart.',
+      },
+      {
+        id: '005',
+        text:
+          "On the beautiful and auspicious festival of Govardhan Puja, we wish you a very happy and prosperous day filled with Lord Krishna's blessings.",
+      },
+      {
+        id: '006',
+        text:
+          'May this special day of Govardhan Puja bring you success, wealth and lots of love.',
+      },
+      {
+        id: '007',
+        text:
+          'Govardhan Puja is an auspicious day filled with faith and prayers. May this day bring in your life all the things that are dear to you. Happy Govardhan Puja.',
+      },
+      {
+        id: '008',
+        text:
+          'On this auspicious day of Govardhan Puja, warm wishes to you and your family!',
+      },
+      {
+        id: '009',
+        text:
+          'Let us learn from Lord Krishna the art of sharing sorrows. Happy Govardhan puja to you and your family!',
+      },
+      {
+        id: '010',
+        text:
+          'May Shri Krishna bless you and your family with an abundance of health & wealth!',
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'कृष्ण की शरण में आकर भक्त नया जीवन पाते है इसलिए गोवर्धन पूजा का दिन हम सच्चे मन से मनाते है हैप्पी गोवर्धन पूजा..',
+      },
+      {
+        id: '002',
+        text:
+          'प्रेम से जपो कृष्ण का नाम, पूरे होंगे सारे अधूरे काम, आज काम न करना कोई दूजा, आज तो करना है गोवर्धन पूजा..',
+      },
+      {
+        id: '003',
+        text:
+          'कृष्ण की भक्ति और दिल में रहे प्यार, मुबारक हो गोवर्धन पूजा का त्यौहार Happy Govardhan Puja..',
+      },
+      {
+        id: '004',
+        text: 'हैप्पी गोवर्धन पूजा..',
+      },
+      {
+        id: '005',
+        text:
+          'कृष्ण की भक्ति और दिल में रहे प्यार मुबारक हो गोवर्धन पूजा का त्यौहार. Happy Govardhan Puja..',
+      },
+      {
+        id: '006',
+        text:
+          'मेरे प्रभु श्री कृष्ण जिनका है नाम, गोकुल जिनका है धाम, ऐसे प्रभु श्री कृष्ण जी को हम सब करें प्रणाम. हैप्पी गोवर्धन पूजा..',
+      },
+      {
+        id: '007',
+        text:
+          'लोगों की रक्षा करने एक ऊँगली पर पहाड़ उठाया, उसी कन्हैया की याद दिलाने गोवर्धन पूजा का पावन दिन आया..',
+      },
+      {
+        id: '008',
+        text:
+          'कृष्ण की शरण में आकर, भक्त नया जीवन पाते हैं, इसलिए गोवर्धन पूजा का दिन हम सच्चे मन से मनाते हैं! हैप्पी गोवर्धन पूजा!..',
+      },
+      {
+        id: '009',
+        text: 'गोवर्धन पूजा में कृष्ण गुण गाये, और ये त्यौहार खुशी से मनाएं!..',
+      },
+      {
+        id: '010',
+        text:
+          'गोवर्धन पूजा के इस पावन अवसर पर आपको और आपके परिवार को ढेरो शुभकामनाए..',
+      },
+    ]
+  }
+
+
+};
